915f59089d0007ed1364419268d5048c90aa605c
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-12-17 22:10:28 +0530

    chore: add timer
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/king-11/AdventOfCode/day15"
 )
 
 func main() {
-  val, err := day15.Solve("./day15/data.txt", true)
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println(val)
+	then := time.Now()
+	val, err := day15.Solve("./day15/data.txt", true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Time Taken: ", time.Since(then))
+	fmt.Println("Answer: ", val)
 }
