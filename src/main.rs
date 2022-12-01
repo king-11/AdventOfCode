c2ba6d5ea43618aaee73187fee98ab7d5a7e3e06
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-12-01 16:31:32 +0530

    chore: add benchmarking
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,9 @@ mod day1;
 
 fn main() {
     let content =  fs::read_to_string("data/day1.txt").unwrap();
-    println!("Answer {}", day1::part2(&content));
+    let t0 = std::time::Instant::now();
+    let result = day1::part2(&content);
+    let t1 = std::time::Instant::now().duration_since(t0);
+    println!("Answer {}", result);
+    println!("Time used: ({:?})", t1);
 }
