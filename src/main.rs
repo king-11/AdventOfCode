291475cacd45f5349d417d4c96b4f22f3bddf48f
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-12-09 13:38:14 +0530

    chore: cleanup
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::fs;
-
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 
 fn main() {
-    let content =  fs::read_to_string("data/day3.txt").unwrap();
+    let content =  include_str!("../data/day4.txt");
     let t0 = std::time::Instant::now();
     let result = day4::part2(&content);
     let t1 = std::time::Instant::now().duration_since(t0);
